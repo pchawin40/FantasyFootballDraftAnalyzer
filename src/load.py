9085835748e27d1ffd44d13e486a9152d8e0c65f
Chawin Pathompornvivat <p.chawin40@gmail.com>
2026-06-20 17:56:42 -0700

    Loaded available players after draft picked | 5:56 PM 6.20.26 HP

--- src/load.py
+++ src/load.py
@@ -61,7 +61,10 @@ def load_sample_players():
             """
         ).fetchdf()
 
+        # print sample players result
+        print("Sample Players Result:")
         print(result)
+        print("--------------------------------")
 
 def load_draft_picks():
     """
@@ -93,7 +96,7 @@ def load_draft_picks():
 
         con.execute("INSERT INTO fact_draft_pick SELECT * FROM draft_players_df")
 
-        result = con.execute(
+        draft_picks_result = con.execute(
             """
             SELECT
                 fdp.draft_pick_id,
@@ -106,8 +109,67 @@ def load_draft_picks():
             """
         ).fetchdf()
 
-        print(result)
+        # print draft picks result
+        print("Draft Players Result:")
+        print(draft_picks_result)
+        print("--------------------------------")
+
+def load_available_players():
+    """
+    load available players from DuckDB database
+    """
+    # read available players from CSV file
+    df = pd.read_csv(PLAYERS_PATH)
+
+    df["season"] = 2026
+    df["vorp"] = df["projected_points"] - df["replacement_points"]
+
+    # create dataframe for available players
+    available_players_df = df [["player_id", "player_name", "position", "nfl_team"]]
+
+    available_players_projections_df = df [
+        [
+            "player_id",
+            "season",
+            "projected_points",
+            "replacement_points",
+            "vorp",
+        ]
+    ].copy()
+
+    available_players_projections_df.insert(0, "projection_id", range(1, len(available_players_projections_df) + 1))
+
+    # start up database connection
+    with duckdb.connect(DB_PATH) as con:
+        schema_sql = SCHEMA_PATH.read_text()
+        con.execute(schema_sql)
+
+        con.register("available_players_df",  available_players_df)
+        con.register("projections_df", available_players_projections_df)
+
+        available_players_result = con.execute(
+            """
+            SELECT
+                ap.player_name,
+                ap.position,
+                ap.nfl_team,
+                fp.projected_points,
+                fp.replacement_points,
+                fp.vorp
+            FROM dim_player ap
+            JOIN fact_player_projection fp USING (player_id)
+            LEFT JOIN fact_draft_pick fdp USING (player_id)
+            WHERE fdp.player_id IS NULL
+            ORDER BY fp.vorp DESC
+            """
+        ).fetchdf()
+
+        # print available players result
+        print("Available Players:")
+        print(available_players_result)
+        print("--------------------------------")
 
 if __name__ == "__main__":
     load_sample_players()
     load_draft_picks()
+    load_available_players()
